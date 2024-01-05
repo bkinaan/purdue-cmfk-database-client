7e53be88e9d627037cecd16e58562c9b65bb9b23
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2024-01-05 13:36:21 -0800

    updated mentor interface to include roles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import fs from "fs";
+import csv from "csv-parser";
 
 const inter = Inter({ subsets: ["latin"] });
 
-interface User {
+interface Mentor {
   id: number;
   first_name: string;
   last_name: string;
@@ -13,34 +15,36 @@ interface User {
   non_school_email: string;
   phone: string;
   activity_days: string;
+  role: string;
+  second_role: string;
   paired_with: string;
 }
 
 export default function Home() {
-  const [users, setUsers] = useState<User[]>([]);
+  const [mentors, setMentors] = useState<Mentor[]>([]);
 
   useEffect(() => {
-    async function fetchUsers() {
+    async function fetchMentors() {
       try {
         const response = await axios.get(
           "http://localhost:8080/api/v1/mentors"
         );
-        setUsers(response.data);
+        setMentors(response.data);
       } catch (error) {
         console.error(error);
       }
     }
 
-    fetchUsers();
+    fetchMentors();
   }, []);
 
   return (
     <div>
-      <h1>Users</h1>
+      <h1>Mentors</h1>
       <ul className="text-white">
-        {users.map((user: User) => (
-          <li key={user.id}>
-            {user.first_name} {user.last_name}
+        {mentors.map((mentor: Mentor) => (
+          <li key={mentor.id}>
+            {mentor.first_name} {mentor.last_name}
           </li>
         ))}
       </ul>
