d2a12198aebe7b1ac7ed766827f58f02b4b4f3eb
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2024-02-04 23:52:48 -0500

    updated homepage table

--- pages/mentors/Table.tsx
+++ pages/mentors/Table.tsx
@@ -1,160 +1,29 @@
 import { useEffect, useState } from "react";
 import { Mentor, columns } from "./columns";
 import { DataTable } from "./data-table";
+import axios from "axios";
 
-function getData(): Mentor[] {
+async function getData(): Promise<Mentor[]> {
   // TODO: fetch data
-  return [];
+  let response;
+  try {
+    const jwt = sessionStorage.getItem("jwt");
+    response = await axios.get("http://localhost:8080/api/v1/mentors", {
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 }
 
-// export default async function Page() {
 export default function Table() {
-  const data: Mentor[] = [
-    {
-      id: 1,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 2,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 3,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 4,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 5,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 6,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 7,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 8,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-    {
-      id: 9,
-      FirstName: "Bob",
-      LastName: "Bobby",
-      EmailAddress: "email@address.com",
-      NonSchoolEmailAddress: "other@website.com",
-      Phone: "2352353335",
-      ActivityDays: "Tuesday",
-      PrimaryStaffRole: "VP of Programming",
-      SecondaryStaffRole: "",
-      Paired: "Buddy Schmody",
-    },
-  ];
-  //   const data = await getData();
+  const [data, setData] = useState<Mentor[]>([]);
 
-  //   return (
-  //     <div className="container mx-auto py-10">
-  //       <DataTable columns={columns} data={data}></DataTable>
-  //     </div>
-  //   );
-  // const [mentors, setMentors] = useState<Mentor[]>([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const data = await getData();
-  //     setMentors(data);
-  //   }
-
-  //   fetchData();
-  // }, []);
-
-  // if (mentors === null) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // const data = getData();
-
-  const mentors = data.map((mentor) => ({
-    id: mentor.id,
-    FirstName: mentor.FirstName,
-    LastName: mentor.LastName,
-    EmailAddress: mentor.EmailAddress,
-    NonSchoolEmailAddress: mentor.NonSchoolEmailAddress,
-    Phone: mentor.Phone,
-    ActivityDays: mentor.ActivityDays,
-    PrimaryStaffRole: mentor.PrimaryStaffRole,
-    SecondaryStaffRole: mentor.SecondaryStaffRole,
-    Paired: mentor.Paired,
-  }));
+  useEffect(() => {
+    getData().then(setData);
+  }, []);
 
   return (
     <div className="container mx-auto py-10">
