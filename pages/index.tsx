739562220cc7315d92028c7d0e38c7c99f90fc5b
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2024-06-16 18:06:09 -0700

    added token verification and made small ui changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,19 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { jwtDecode } from "jwt-decode";
 
 export default function Home() {
   const router = useRouter();
 
   const handleLoginClick = () => {
-    const jwt = localStorage.getItem("jwt");
-
-    if (jwt) {
-      const decoded = jwtDecode(jwt);
-      const date = new Date();
-
-      if ((decoded.exp ?? 0) * 1000 < date.getTime()) {
-        console.log("expired");
-        router.push("/login");
-      } else {
-        console.log("token is all good!");
-
-        router.push("/dashboard");
-      }
-    } else {
-      router.push("/login");
-    }
+    router.push("/login");
   };
 
   const handleSignUpClick = () => {
     router.push("/signup");
   };
+
   return (
-    <div className="font-montserrat flex items-center pt-48 text-center">
+    <div className="font-montserrat bg-grey flex items-center pt-48 text-center">
       <div className="text-teal w-2/3 p-4 text-7xl font-bold">
         <div className="m-auto max-w-md text-center">
           <div className="text-orange">Purdue</div>
@@ -40,13 +24,13 @@ export default function Home() {
       <div className="text-grey '#EAE3D9' mt-8 flex w-1/3 max-w-fit flex-col items-center justify-center rounded-xl p-12 text-2xl">
         <button
           onClick={handleLoginClick}
-          className="bg-teal hover:bg-orange shadow-grey hover:shadow-grey my-8 rounded-xl px-4 py-2 shadow-lg transition duration-300 ease-in-out hover:translate-y-1 hover:shadow-md"
+          className="bg-teal hover:bg-orange shadow-teal hover:shadow-orange my-8 rounded-xl px-4 py-2 shadow-md transition duration-300 ease-in-out hover:translate-y-1 hover:shadow-sm"
         >
           Log In
         </button>
         <button
           onClick={handleSignUpClick}
-          className="bg-teal hover:bg-orange shadow-grey hover:shadow-grey mb-8 rounded-xl px-4 py-2 shadow-lg transition duration-300 ease-in-out hover:translate-y-1 hover:shadow-md"
+          className="bg-teal hover:bg-orange shadow-teal hover:shadow-orange mb-8 rounded-xl px-4 py-2 shadow-md transition duration-300 ease-in-out hover:translate-y-1 hover:shadow-sm"
         >
           Sign Up
         </button>
